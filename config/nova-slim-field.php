562c0db16bf802d87c8a8c25bb68a5649f7f095e
Marius <ExpDev07@users.noreply.github.com>
2022-02-03 15:31:14 +0100

    Update nova-slim-field.php

--- config/nova-slim-field.php
+++ config/nova-slim-field.php
@@ -5,8 +5,8 @@ return [
      * Configure the path to the slim assets.
      */
     'slim' => [
-        'jquery' => asset('/js/jquery.js'),
-        'js' => asset('/js/slim.js'),
-        'css' => asset('/css/slim.css'),
+        'jquery' => public_path('/js/jquery.js'),
+        'js' => public_path('/js/slim.js'),
+        'css' => public_path('/css/slim.css'),
     ],
 ];
